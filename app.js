e99aa41718504eb619e99051a3eec52a2f71f35f
mcmak <maimcmak@outlook.com>
2022-10-06 18:41:45 +0800

    Blog list 4.15-4.16

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middware = require('./utils/middleware')
@@ -22,9 +23,11 @@ app.use(cors())
 app.use(express.json())
 
 app.use(middware.requestLogger)
-app.use(middware.errorHandler)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+app.use(middware.errorHandler)
 
 
 module.exports = app
